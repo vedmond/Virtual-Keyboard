56c4bbcd7bb890bc94c825daa62fa399fb24aaa6
VedKhorkov <vyedkh@gmail.com>
2022-05-06 10:15:23 +0300

    feat: add build row one

--- js/rowForKey.js
+++ js/rowForKey.js
@@ -1,8 +1,10 @@
 const rowForKey = () => {
-  document.querySelector('.keyboard-block').innerHTML = `<div class="row"></div>
+  const rows = document.querySelector('.keyboard-block').innerHTML = `<div class="row"></div>
+               <div class="row"></div>
                <div class="row"></div>
                <div class="row"></div>
                <div class="row"></div>`;
-  return row;
+  return rows;
 };
+
 export default rowForKey;
---
+++ js/rowOne.js
@@ -0,0 +1,30 @@
+import btnKey from "./btnKey.js";
+
+const rowOne = () => {
+  return (`${btnKey('k0', '`', '~', 'colorbg')} 
+  ${btnKey('k1', '1', '!')}
+  ${btnKey('k2', '2', '@')}
+  ${btnKey('k3', '3', '#')}
+  ${btnKey('k4', '4', '$')}
+  ${btnKey('k5', '5', '%')}
+  ${btnKey('k6', '6', ':')}
+  ${btnKey('k7', '7', '?')}
+  ${btnKey('k8', '8', '*')}
+  ${btnKey('k9', '9', '(')}
+  ${btnKey('k10', '0', ')')}
+  ${btnKey('k11', '-', '_')}
+  ${btnKey('k12', '=', '+')}
+  ${btnKey('k13', 'Backspase', '', 'colorbg', '110px')}
+  
+  `
+
+  );
+}
+console.log(rowOne());
+export default rowOne;
+
+
+
+
+
+// const btnKey = (id, item = '', itemtwo = '', colorbg = '', width = '50px')
